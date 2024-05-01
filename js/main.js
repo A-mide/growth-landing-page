a21f4c95620efcdf4389981cabfbfb6b98e27652
A-mide <odeleayomide3@gmail.com>
2024-05-01 20:12:47 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+// faq
+
+document.addEventListener('DOMContentLoaded', () => {
+  const faqContainer = document.querySelector('.faq-content');
+
+  faqContainer.addEventListener('click', (e) => {
+    const groupHeader = e.target.closest('.faq-group-header');
+
+    if (!groupHeader) return;
+
+    const group = groupHeader.parentElement;
+    const groupBody = group.querySelector('.faq-group-body');
+    const icon = groupHeader.querySelector('i');
+
+    // TOGGLE ICON
+    icon.classList.toggle('fa-plus');
+    icon.classList.toggle('fa-minus');
+
+    // TOGGLE VISIBILITY OF ReportBody
+    groupBody.classList.toggle('open');
+
+    // CLOSE OTHER FAQ BODIES
+
+    const otherGroups = faqContainer.querySelectorAll('.faq-group');
+
+    otherGroups.forEach((otherGroup) => {
+      if (otherGroup !== group) {
+        const otherGroupBody = otherGroup.querySelector('.faq-group-body');
+        const otherIcon = otherGroup.querySelector('.faq-group-header i');
+
+        otherGroupBody.classList.remove('open');
+        otherIcon.classList.remove('fa-minus');
+        otherIcon.classList.add('fa-plus');
+      }
+    });
+  });
+});
+
+// Mobile MEnu
+
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburgerButton = document.querySelector('.hamburger-button');
+  const mobileMenu = document.querySelector('.mobile-menu');
+
+  hamburgerButton.addEventListener('click', () =>
+    mobileMenu.classList.toggle('active')
+  );
+});
